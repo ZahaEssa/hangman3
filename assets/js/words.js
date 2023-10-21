a2888960585ff2569378bd790cc316d86f197685
ZahaEssa <shamoonzahabiy@gmail.com>
2023-10-21 14:54:19 +0300

    Added assets folder

---
+++ assets/js/words.js
@@ -0,0 +1,18 @@
+
+var categories = {
+  easy: {
+    Colors: ["RED", "GREEN", "BLUE", "YELLOW", "ORANGE"],
+    Fruits: ["APPLE", "BANANA", "ORANGE", "STRAWBERRY", "KIWI"],
+    Animals: ["DOG", "CAT", "LION", "ELEPHANT", "TIGER"]
+  },
+  medium: {
+    Movies: ["AVATAR", "INCEPTION", "GLADIATOR", "PULP FICTION", "TITANIC"],
+    VideoGames: ["MARIO", "ZELDA", "SONIC", "PAC-MAN", "FORTNITE"],
+    Countries: ["USA", "CANADA", "FRANCE", "JAPAN", "BRAZIL"]
+  },
+  hard: {
+    FamousNovels: ["TO KILL A MOCKINGBIRD", "WAR AND PEACE", "THE GREAT GATSBY", "MOBY-DICK", "Ulysses"],
+    HistoricalFigures: ["LEONARDO DA VINCI", "WILLIAM SHAKESPEARE", "ALBERT EINSTEIN", "MARTIN LUTHER KING JR.", "GALILEO GALILEI"],
+    Scientists: ["ISAAC NEWTON", "MARIE CURIE", "CHARLES DARWIN", "STEPHEN HAWKING", "NIKOLA TESLA"]
+  }
+};
