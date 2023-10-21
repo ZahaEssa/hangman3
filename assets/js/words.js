a13abc116cd69adea494ffc97d9fca9a5d6fef01
ZahaEssa <shamoonzahabiy@gmail.com>
2023-10-21 15:02:11 +0300

    Updated words file and javascript file

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,18 +1,11 @@
-
 var categories = {
   easy: {
-    Colors: ["RED", "GREEN", "BLUE", "YELLOW", "ORANGE"],
-    Fruits: ["APPLE", "BANANA", "ORANGE", "STRAWBERRY", "KIWI"],
-    Animals: ["DOG", "CAT", "LION", "ELEPHANT", "TIGER"]
+    Colors: ["RED", "GREEN", "BLUE", "YELLOW", "ORANGE"]
   },
   medium: {
-    Movies: ["AVATAR", "INCEPTION", "GLADIATOR", "PULP FICTION", "TITANIC"],
-    VideoGames: ["MARIO", "ZELDA", "SONIC", "PAC-MAN", "FORTNITE"],
-    Countries: ["USA", "CANADA", "FRANCE", "JAPAN", "BRAZIL"]
+    Movies: ["AVATAR", "INCEPTION", "GLADIATOR", "PULP FICTION", "TITANIC"]
   },
   hard: {
-    FamousNovels: ["TO KILL A MOCKINGBIRD", "WAR AND PEACE", "THE GREAT GATSBY", "MOBY-DICK", "Ulysses"],
-    HistoricalFigures: ["LEONARDO DA VINCI", "WILLIAM SHAKESPEARE", "ALBERT EINSTEIN", "MARTIN LUTHER KING JR.", "GALILEO GALILEI"],
-    Scientists: ["ISAAC NEWTON", "MARIE CURIE", "CHARLES DARWIN", "STEPHEN HAWKING", "NIKOLA TESLA"]
+    FamousNovels: ["TO KILL A MOCKINGBIRD", "WAR AND PEACE", "THE GREAT GATSBY", "MOBY-DICK", "Ulysses"]
   }
 };
