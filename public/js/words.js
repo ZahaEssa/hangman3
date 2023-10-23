2f983038b17a43e1e55438fbc24bdb7760e7e11e
ZahaEssa <shamoonzahabiy@gmail.com>
2023-10-23 18:05:18 +0300

    added public  folder

---
+++ public/js/words.js
@@ -0,0 +1,12 @@
+var categories = {
+    easy: {
+      Colors: ["RED", "GREEN", "BLUE", "YELLOW", "ORANGE"]
+    },
+    medium: {
+      Movies: ["AVATAR", "INCEPTION", "GLADIATOR", "PULP FICTION", "TITANIC"]
+    },
+    hard: {
+      FamousNovels: ["TO KILL A MOCKINGBIRD", "WAR AND PEACE", "THE GREAT GATSBY", "MOBY-DICK", "Ulysses"]
+    }
+  };
+  
