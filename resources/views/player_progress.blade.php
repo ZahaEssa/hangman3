0a119d1ce3df7eff0d8e0a4563f773d9c71e63f4
Charles Maina <charlesmaina2002@gmail.com>
2023-11-22 14:44:15 +0300

    added the exportTop and exportExcel for the line chart

--- resources/views/player_progress.blade.php
+++ resources/views/player_progress.blade.php
@@ -8,8 +8,9 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <!-- Include html2pdf library -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
     <title>User Progress</title>
- 
+
 </head>
 
 <body>
@@ -24,6 +25,8 @@
         <a href="{{ route('progress') }}">Back to Progress</a>
         <a href="{{ route('progress_graph') }}">View Pie Chart</a>
         <a href="{{ route('progress_chart') }}">View Bar Chart</a>
+        <button onclick="exportToPDF()">Export to PDF</button>
+        <button onclick="exportToExcel()">Export to Excel</button>
     </div>
     {{-- Get the username from the session --}}
     @php $username = Session::get('data'); @endphp
@@ -45,7 +48,7 @@
     {{-- If there are results, display a line chart --}}
     @if(count($groupedResult) > 0)
     <h1 style="text-align: center;white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ $username }}'s Progress Over Time</h1>
-    <canvas id="lineChart" width="1000" height="300" <h1 style="text-align: center;white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ $username }}'s Progress Over Time</h1>></canvas>
+    <canvas id="lineChart" width="1000" height="300"></canvas>
     <script>
         var groupedData = <?php echo json_encode($groupedResult); ?>;
         var labels = groupedData.map(item => item.created_at);
@@ -84,6 +87,36 @@
                 }
             }
         });
+        function exportToPDF() {
+    // Hide navigation buttons before exporting
+    var navigationButtons = document.querySelector('.navigation');
+    navigationButtons.style.display = 'none';
+
+    // Get the chart container for export
+    var chartContainer = document.getElementById('lineChart').parentElement;
+
+    // Use html2pdf to export the chart container
+    html2pdf(chartContainer, {
+        margin: 10,
+        filename: 'user_progress.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        onBeforeDownload: function () {
+            // Restore visibility of navigation buttons after export
+            navigationButtons.style.display = 'block';
+        }
+    });
+}
+
+
+        function exportToExcel() {
+            var wb = XLSX.utils.book_new();
+            var ws = XLSX.utils.json_to_sheet(groupedData);
+
+            XLSX.utils.book_append_sheet(wb, ws, 'Progress Data');
+            XLSX.writeFile(wb, 'progress_data.xlsx');
+        }
     </script>
     @else
     <p>No results found for this user.</p>
